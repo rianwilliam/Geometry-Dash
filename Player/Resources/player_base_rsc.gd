ce9a4a1938e4c921d60eb1ffb4a42cd2656ae912
Rian William <rianwilliammonteiro@gmail.com>
2025-08-18 13:43:09 -0300

    resource implementation

--- Player/Resources/player_base_rsc.gd
+++ Player/Resources/player_base_rsc.gd
@@ -3,4 +3,3 @@ class_name PlayerBaseResource
 
 @export var speed: float = 170.0
 @export var gravity: float = 15.0
-@export var gravity_dir: Enums.GRAVITY_DIR = Enums.GRAVITY_DIR.NORMAL
--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -6,14 +6,17 @@ class_name Player
 @onready var spawn: Marker2D = %PlayerSpawn
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var _gravity_dir: Enums.GRAVITY_DIR = Enums.GRAVITY_DIR.NORMAL
 
 var _action_pressed: bool
 var _mode: Enums.PLAYER_MODE = Enums.PLAYER_MODE.SQUARE
 var _spaceship_vertical_vel: float = -15.0
 var _player_base_rsc: PlayerBaseResource = preload("res://Player/Resources/player_base_rsc.tres")
 var _square_rsc: SquareResource = preload("res://Player/Resources/square_rsc.tres")
-var wave_rsc: WaveResource = preload("res://Player/Resources/wave_rsc.tres")
-var ufo_rsc: UfoResource = preload("res://Player/Resources/ufo_rsc.tres")
+var _wave_rsc: WaveResource = preload("res://Player/Resources/wave_rsc.tres")
+var _ufo_rsc: UfoResource = preload("res://Player/Resources/ufo_rsc.tres")
+var _ball_rsc: BallResource = preload("res://Player/Resources/ball_rsc.tres")
+var _spaceship_rsc: SpaceshipResource = preload("res://Player/Resources/spaceship_rsc.tres")
 
 func _ready() -> void:
 	global_position = spawn.global_position
@@ -33,26 +36,26 @@ func _physics_process(_delta: float) -> void:
 
 func _square_mode() -> void:
 	if not is_on_floor() or not is_on_ceiling():
-		_apply_gravity()
+		_apply_gravity(_square_rsc)
 
 	if _action_pressed and is_on_floor() or _action_pressed and is_on_ceiling():
 		_jump()
 		animation_player.play("rotate")
 
 func _jump() -> void:
-	velocity.y = _square_rsc.jump_height * _player_base_rsc.gravity_dir
+	velocity.y = _square_rsc.jump_height * _gravity_dir
 
 func change_mode(new_mode: Enums.PLAYER_MODE) -> void:
 	_mode = new_mode
 
-func _apply_gravity() -> void:
-	velocity.y += _player_base_rsc.gravity * _player_base_rsc.gravity_dir
+func _apply_gravity(rsc: PlayerBaseResource) -> void:
+	velocity.y += rsc.gravity * _gravity_dir
 
 func invert_gravity() -> void:
-	if _player_base_rsc.gravity_dir == Enums.GRAVITY_DIR.NORMAL:
-		_player_base_rsc.gravity_dir = Enums.GRAVITY_DIR.INVERTED
+	if _gravity_dir == Enums.GRAVITY_DIR.NORMAL:
+		_gravity_dir = Enums.GRAVITY_DIR.INVERTED
 	else:
-		_player_base_rsc.gravity_dir = Enums.GRAVITY_DIR.NORMAL
+		_gravity_dir = Enums.GRAVITY_DIR.NORMAL
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	Events.emit_signal("player_died")
@@ -60,7 +63,7 @@ func _on_hurt_box_body_entered(body: Node2D) -> void:
 
 #region Wave
 func _wave_mode() -> void:
-	velocity.y = wave_rsc.vertical_speed * wave_rsc.direction
+	velocity.y = _wave_rsc.vertical_speed * _wave_rsc.direction * _gravity_dir
 	if _action_pressed:
 		_change_wave_direction(Enums.WAVE_DIR.UP)
 		_add_trial_point()
@@ -72,21 +75,21 @@ func _add_trial_point() -> void:
 	wave_trial.add_point(global_position)
 
 func _change_wave_direction(new_dir: Enums.WAVE_DIR) -> void:
-	wave_rsc.direction = new_dir
+	_wave_rsc.direction = new_dir
 #endregion
 
 #region Ball
 func _ball_mode() -> void:
 	if _action_pressed and is_on_ceiling(): invert_gravity()
 	elif _action_pressed and is_on_floor(): invert_gravity()
-	_apply_gravity()
+	_apply_gravity(_ball_rsc)
 #endregion
 
 #region Ufo
 func _ufo_mode() -> void:
-	_apply_gravity()
+	_apply_gravity(_ufo_rsc)
 	if Input.is_action_just_pressed("Action"):
-		velocity.y = ufo_rsc.jump_height
+		velocity.y = _ufo_rsc.jump_height * _gravity_dir
 #endregion
 
 #region Reset
@@ -98,7 +101,7 @@ func _reset_player() -> void:
 #region Spaceship
 func _spaceship_mode() -> void:
 	if not _action_pressed:
-		_apply_gravity()
+		_apply_gravity(_spaceship_rsc)
 	else:
-		velocity.y += _spaceship_vertical_vel
+		velocity.y += _spaceship_vertical_vel * _gravity_dir
 #endregion
