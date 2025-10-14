f2d7cba502e85f7732aa7338279ae6bd6864fbd3
Rian-Educacao <rianwilliameducacao@gmail.com>
2025-10-14 14:52:13 -0300

    Docstrings finished

--- Ui/Menus/LevelsMenu/Scripts/levels_menu.gd
+++ Ui/Menus/LevelsMenu/Scripts/levels_menu.gd
@@ -1,18 +1,23 @@
 extends Control
 class_name LevelsMenu
 
-@onready var lvl_1_btn: Button = %Lvl1Btn
-@onready var lvl_2_btn: Button = %Lvl2Btn
-@onready var lvl_3_btn: Button = %Lvl3Btn
+## Aba responsável pela seleção de níveis
+##
+##
+
+@onready var lvl_1_btn: Button = %Lvl1Btn ## Botão responsável por mudar para a cena do nível 1 
+@onready var lvl_2_btn: Button = %Lvl2Btn ## Botão responsável por mudar para a cena do nível 2
+@onready var lvl_3_btn: Button = %Lvl3Btn ## Botão responsável por mudar para a cena do nível 3
 
 func _ready() -> void:
 	_connect_events()
-	
+
+## Conecta os eventos de clique dos botões
 func _connect_events() -> void:
 	lvl_1_btn.connect("pressed", _on_lvl_1_btn_pressed)
 	lvl_2_btn.connect("pressed", _on_lvl_2_btn_pressed)
 	lvl_3_btn.connect("pressed", _on_lvl_3_btn_pressed)
-	
+
 func _on_lvl_1_btn_pressed() -> void:
 	SceneChanger.change_scene(Enums.SCENES.LEVEL_ONE)
 
--- Ui/Menus/PauseMenu/Scripts/pause_menu.gd
+++ Ui/Menus/PauseMenu/Scripts/pause_menu.gd
@@ -1,22 +1,33 @@
 extends Control
 class_name PauseMenu
 
-@onready var resume_btn: Button = %ResumeBtn
-@onready var quit_btn: Button = %QuitBtn
+## Menu displayed when the player presses the pause button
+##
+## Contains options to either resume the game or quit back to the [LevelsMenu].
+
+@onready var resume_btn: Button = %ResumeBtn ## When pressed, resumes the game
+@onready var quit_btn: Button = %QuitBtn ## When pressed, returns to the [LevelsMenu]
 
 func _ready() -> void:
-	size = get_viewport().get_visible_rect().size
 	resume_btn.connect("pressed", _on_resume_btn_pressed)
 	quit_btn.connect("pressed", _on_quit_btn_pressed)
 
+## Adjusts the menu size to match the current viewport size
+func _resize_menu_to_viewport() -> void:
+	size = get_viewport().get_visible_rect().size
+
+## Emits a signal indicating that the resume button was pressed
 func _on_resume_btn_pressed() -> void:
 	Events.emit_signal("resume_btn_pressed")
 
+## Emits a signal indicating that the quit button was pressed
 func _on_quit_btn_pressed() -> void:
 	Events.emit_signal("quit_btn_pressed")
 
+## Makes the pause menu visible
 func show_pause() -> void:
 	visible = true
 
+## Hides the pause menu
 func hide_pause() -> void:
 	visible = false
