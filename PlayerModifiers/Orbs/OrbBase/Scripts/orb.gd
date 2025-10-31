43631cee8688a5b9f1ad54b5128a57c4f0cb14bc
Rian William <rianwilliammonteiro@gmail.com>
2025-10-30 21:29:01 -0300

    Update level one

--- PlayerModifiers/Orbs/OrbBase/Scripts/orb.gd
+++ PlayerModifiers/Orbs/OrbBase/Scripts/orb.gd
@@ -2,9 +2,12 @@ extends PlayerModifier
 class_name Orb
 
 @onready var orb_layers: OrbLayers = $OrbLayers
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	Events.connect("player_use_orb", _on_player_use_orb)
 	
 func _on_player_use_orb() -> void:
-	if player_in_area: orb_layers.play_animations()
+	if player_in_area: 
+		orb_layers.play_animations()
+		collision_shape_2d.disabled = true
